dc270c8f5b6161ed2d74ad49739aff38d6f39233
showframework <showframework@gmail.com>
2012-09-11 03:30:31 +0000

    delete useless

--- js/five.js
+++ js/five.js
@@ -5,10 +5,4 @@ Array.prototype.coordinate = function (coordinate) {
     }
     return r;
 };
-Array.prototype.coordinateAdd = function (coordinate) {
-    return [this[0] + coordinate[0], this[1] + coordinate[1]];
-};
-var inside = function (coordinate, width, height) {
-    return coordinate[0] >= 0 && coordinate[0] < height && coordinate[1] >= 0 && coordinate[1] < width;
-};
 
