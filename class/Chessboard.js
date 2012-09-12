f23076b7b2957eac053ff9e32595dbb771464ee0
showframework <showframework@gmail.com>
2012-09-12 06:50:46 +0000

    显示走棋日志

--- class/Chessboard.js
+++ class/Chessboard.js
@@ -46,16 +46,6 @@ var Chessboard = function (size) {
  */
 Chessboard.prototype.wait = function () {
     this.waiting = true;
-    var that = this;
-    this.el.delegate('td', 'click', function () {
-        if (that.waiting) {
-            that.waiting = false;
-            var i = jQuery(this).data('i');
-            var j = jQuery(this).data('j');
-            console.log('用户下棋', [i, j], that.getDomByCoordinate([i, j]));
-            that.go([i, j], that.turn);
-        }
-    });
 };
 
 /**
@@ -65,28 +55,37 @@ Chessboard.prototype.doNothing = function () {
 };
 
 /**
- * 下一步棋，记录日志，并转交先手
+ * 记录走棋日志
  * @param {Array} coordinate
  * @param {String} color
  */
-Chessboard.prototype.go = function (coordinate, color) {
+Chessboard.prototype.doLog = function (coordinate, color) {
     //记录日志
     this.log.push({
         coordinate: coordinate,
         color: color
     });
     jQuery('#log').append('<div><span class="log-coordinate">(' + coordinate[0] + ',' + coordinate[1] + ')</span> <span>' + color + '</span></div>');
+};
 
+/**
+ * 下一步棋，记录日志，并转交先手
+ * @param {Array} coordinate
+ * @param {String} color
+ */
+Chessboard.prototype.go = function (coordinate, color) {
+    var that = this;
     var value = (color == 'black' ? 3 : 1);
     if (this.matrix.getValueByCoordinate(coordinate) === 0) {
         this.matrix.setValueByCoordinate(coordinate, value);
         this.el.find('tr:eq(' + coordinate[0] + ')').find('td:eq(' + coordinate[1] + ')').append('<div class="pieces ' + color + '"></div>');
-        var that = this;
+        this.doLog(coordinate, color);
         setTimeout(function () {
             that.changeTurn();
         }, 10);
     } else {
         alert(coordinate + ' 这个点已经有棋子了');
+        this.wait();
     }
 };
 
@@ -117,6 +116,15 @@ Chessboard.prototype.render = function (renderTo) {
     html.push('</table>');
     this.el = jQuery(html.join(''));
     jQuery(renderTo).append(this.el);
+    var that = this;
+    this.el.delegate('td', 'click', function () {
+        if (that.waiting) {
+            that.waiting = false;
+            var i = jQuery(this).data('i');
+            var j = jQuery(this).data('j');
+            that.go([i, j], that.turn);
+        }
+    });
 };
 
 /**
