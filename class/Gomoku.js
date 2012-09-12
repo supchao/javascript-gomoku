89d4e218754f8189e36121664e739e0190485b2e
showframework <showframework@gmail.com>
2012-09-12 05:54:19 +0000

    完善性改造

--- class/Gomoku.js
+++ class/Gomoku.js
@@ -6,12 +6,47 @@
  * To change this template use File | Settings | File Templates.
  */
 
+/**
+ * 五子棋
+ * @constructor
+ * @class Gomoku
+ */
 var Gomoku = function () {
+    this.chessboard = new Chessboard(15);
+    this.player1 = new Person(); //把player1当做外来棋手，现在只是人，以后可能是别人编制的AI
+    this.player2 = new Ai();
 
+    //绘制棋盘
+    this.chessboard.render('#container');
 };
-Gomoku.prototype.play = function () {
 
+/**
+ * 开始游戏
+ * @param {String} color 外来棋手的颜色
+ */
+Gomoku.prototype.play = function (color) {
+    //设置人和电脑执棋颜色
+    this.player1.setColor(color);
+    this.player2.setColor(this.changeColor(color));
+
+    this.chessboard.setPlayer(this.player1);
+    this.chessboard.setPlayer(this.player2);
+    this.chessboard.start();
 };
-Gomoku.prototype.replay = function () {
 
+/**
+ * 重新开始游戏
+ * @param {String} color 外来棋手的颜色
+ */
+Gomoku.prototype.replay = function (color) {
+    this.chessboard.restart();
+};
+
+/**
+ * 传入black返回white，传入white返回black
+ * @param {String} color
+ * @return {String}
+ */
+Gomoku.prototype.changeColor = function (color) {
+    return color == 'black' ? 'white' : 'black';
 };
